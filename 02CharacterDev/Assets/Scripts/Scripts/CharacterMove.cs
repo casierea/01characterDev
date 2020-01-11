899f736f48fbb323bce9f79df8d92153c5a961f4
Unknown <dancingmongoose@gmail.com>
2020-01-11 11:31:00 -0700

    character move script 2

---
+++ 02CharacterDev/Assets/Scripts/Scripts/CharacterMove.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+[RequireComponent(typeof(CharacterController))]
+public class CharacterMove : MonoBehaviour
+{
+    private CharacterController Controller;
+
+    private float Gravity = 9.81f;
+    public float MoveSpeed = 3.14f;
+    private Vector3 Rot;
+
+    public Vector3 position;
+    
+    public float Jump = 5f;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        Controller = GetComponent<CharacterController>();
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Controller.isGrounded)
+        {
+            position.Set(MoveSpeed * Input.GetAxis("Horizontal"), 0, MoveSpeed * Input.GetAxis("Vertical"));
+            position = transform.TransformDirection(position);
+        }
+
+        position.y -= Gravity * Time.deltaTime;
+        Controller.Move(position * Time.deltaTime);
+    }
+}
