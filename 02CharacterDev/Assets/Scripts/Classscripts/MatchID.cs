d74975cc2733e8e79a5e63c073a017677a4ae7e0
Unknown <dancingmongoose@gmail.com>
2020-01-15 10:26:36 -0700

    Name Id

---
+++ 02CharacterDev/Assets/Scripts/Classscripts/MatchID.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class MatchID : MonoBehaviour
+{
+    public NameId nameIDObj;
+    
+    private void OnTriggerEnter(Collider other)
+    {
+        var nameIdObj = other.GetComponent<MatchID>().nameIDObj;
+        if (nameIdObj == null) return;
+        var otherNameId = nameIdObj;
+        if (nameIDObj == otherNameId)
+        {
+            //do work
+        }
+    }
+}
