ca89015983b6d6845abcd04ca62d4b0eed184980
Casie <dancingmongoose@gmail.com>
2020-01-16 13:08:10 -0700

    id match scripts

---
+++ 02CharacterDev/Assets/Scripts/Classscripts/DoWork.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DoWork : MonoBehaviour
+{
+    public NameId nameIdObj;
+
+    public void work()
+    {
+        print("Working");
+    }
+}
--- 02CharacterDev/Assets/Scripts/Classscripts/MatchID.cs
+++ 02CharacterDev/Assets/Scripts/Classscripts/MatchID.cs
@@ -1,20 +1,41 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 
 public class MatchID : MonoBehaviour
 {
-    public NameId nameIDObj;
-    
+    public List<NameId> nameIDList;
+
     private void OnTriggerEnter(Collider other)
     {
-        var nameIdObj = other.GetComponent<MatchID>().nameIDObj;
-        if (nameIdObj == null) return;
-        var otherNameId = nameIdObj;
-        if (nameIDObj == otherNameId)
+        var doworkobj = other.GetComponent<DoWork>();
+        var otherNameID = doworkobj.nameIdObj;
+        
+        foreach (var nameId in nameIDList)
         {
-            //do work
+           
+            if (nameId == otherNameID)
+            {
+               
+            }
         }
     }
+    
+    
+    
+
+    //public NameId nameIDObj;
+
+    // private void OnTriggerEnter(Collider other)
+    //{
+    //  var nameIdObj = other.GetComponent<MatchID>().nameIDObj;
+    //  if (nameIdObj == null) return;
+    //  var otherNameId = nameIdObj;
+    //   if (nameIDObj == otherNameId)
+    //  {
+    //do work
+    //  }
+    //}
 }
