0b1b34789054605bf99f8912e568fcff4a00f0f9
Unknown <dancingmongoose@gmail.com>
2020-01-17 13:36:35 -0700

    Trigger event script

--- 02CharacterDev/Assets/Scripts/Classscripts/TriggerEvents.cs
+++ 02CharacterDev/Assets/Scripts/Classscripts/TriggerEvents.cs
@@ -1,18 +1,22 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
 
 public class TriggerEvents : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+   public UnityEvent TriggerEnterEvent, TriggerExitEvent;
+
+   private void OnTriggerEnter(Collider other)
+   {
+      TriggerEnterEvent.Invoke();
+      Debug.Log("again");
+   }
+
+   private void OnTriggerExit(Collider other)
+   {
+      TriggerExitEvent.Invoke();
+      Debug.Log("Exit");
+   }
 }
