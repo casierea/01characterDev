867b280e97a529fc01360a92a8c0b292e5482dcf
Unknown <dancingmongoose@gmail.com>
2020-01-20 18:45:53 -0700

    game action so

--- 02CharacterDev/Assets/Scripts/Scripts/CharacterControl.cs
+++ 02CharacterDev/Assets/Scripts/Scripts/CharacterControl.cs
@@ -12,13 +12,12 @@ public class CharacterControl : MonoBehaviour
     public float Height = 10f;
     public float Radius = 2f;
     public float Offset = .3f;
-    public GameAction action;
     public float Velocity = 4f;
     public Material matieral;
         
     private void Start()
     {
-        controller.GetComponent<CharacterController>();
+//        controller.GetComponent<CharacterController>();
     }
 
     private void SetHeight()
