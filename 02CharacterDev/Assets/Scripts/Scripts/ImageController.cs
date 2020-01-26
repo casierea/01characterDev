e4f49b7f6830b7d6f824308c90a6987b7a45fd4a
Unknown <dancingmongoose@gmail.com>
2020-01-25 21:48:46 -0700

    rewriting scripts for practice.
    imagecontoller, float data, updatevent,

---
+++ 02CharacterDev/Assets/Scripts/Scripts/ImageController.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+[RequireComponent(typeof(Image))]
+public class ImageController : MonoBehaviour
+{
+   private Image imageComponent;
+
+   private void Start()
+   {
+      imageComponent.GetComponent<Image>();
+     
+   }
+
+   public void UpdateImageComponent(float amount)
+   {
+      imageComponent.fillAmount += amount;
+   }
+
+   public void UpdateImageComponent(FloatdataValue dataobj)
+   {
+      imageComponent.fillAmount = dataobj.Value;
+   }
+}
