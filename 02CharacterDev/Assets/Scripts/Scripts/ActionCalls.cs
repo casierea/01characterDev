aa386ac7508f996fb4535b767e4e214d4fd7f79c
Unknown <dancingmongoose@gmail.com>
2020-01-25 21:55:19 -0700

    scripts
    trigger event, mouse events,

---
+++ 02CharacterDev/Assets/Scripts/Scripts/ActionCalls.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class ActionCalls : MonoBehaviour
+{
+    public float speed = 1f, rotateSpeed = 10f, scaleSpeed = 0.02f;
+    private Vector3 location, rotations, scales;
+    private UnityAction transformAction;
+    
+    private void Start()
+    {
+        transformAction = OnMove;
+    }
+    private void OnMouseDown()
+    {
+        if (transformAction == OnMove)
+        {
+            transformAction = OnRotate;
+        }
+		
+        else if (transformAction == OnRotate)
+        {
+            transformAction = OnScale;
+        } 
+		
+        else if (transformAction == OnScale)
+        {
+            transformAction = OnMove;
+        }
+    }
+    void Update ()
+    {
+        transformAction();
+    }
+    public void OnMove()
+    {
+        location.x = speed * Time.deltaTime;
+        transform.Translate(location);
+    }
+    public void OnRotate()
+    {
+        rotations.y = rotateSpeed * Time.deltaTime;
+        transform.Rotate(rotations);
+    }
+    public void OnScale()
+    {
+        scales.Set(scaleSpeed,scaleSpeed,scaleSpeed);
+        transform.localScale += scales;
+    }
+}
