70a1e0c59c0849e1c37581e3373f7073adbe6bc6
Unknown <dancingmongoose@gmail.com>
2020-01-26 14:54:06 -0700

    animation practice with skeleton

---
+++ 02CharacterDev/Assets/Scripts/Scripts/ObjectCollection.cs
@@ -0,0 +1,8 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+[CreateAssetMenu]
+public class ObjectCollection : ScriptableObject
+{
+  public List<ScriptableObject> ObjectList;
+}
