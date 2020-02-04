cf319f9f81c7e1c292658678d44486d563a4f05c
casie <dancingmongoose@gmail.com>
2020-02-04 12:53:12 -0700

    boolfloatdata

---
+++ 02CharacterDev/Assets/CheckBooldata.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CheckBooldata : MonoBehaviour
+{
+   public BooldataObj booldataObj;
+
+   private void Update()
+   {
+      if (booldataObj.value)
+      {
+         print("true"); //do work like maybe fire, power up. if character runs into the wall, stop. (On character controller)
+      }
+      else
+      {
+         //do this instead
+      }
+   }
+}
