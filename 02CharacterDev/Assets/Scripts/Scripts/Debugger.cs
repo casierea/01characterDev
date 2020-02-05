fa12916e55127533d20f62b443328f88a9b63219
Unknown <dancingmongoose@gmail.com>
2020-02-05 10:39:23 -0700

    PlayerMovement Script. redo. practice

---
+++ 02CharacterDev/Assets/Scripts/Scripts/Debugger.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+[CreateAssetMenu]
+public class Debugger : ScriptableObject
+{
+   public void OnDebug (string message)
+   {
+      Debug.Log(message);
+   }
+}
+
---
+++ 02CharacterDev/Assets/Scripts/Scripts/Practice/CharacterControl.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Runtime.InteropServices;
+using UnityEngine;
+[RequireComponent(typeof(CharacterController))]
+public class CharacterControl : MonoBehaviour
+{
+    private Vector3 position;
+    private CharacterController controller;
+
+    
+    public float Height = 10f;
+    public float Radius = 2f;
+    public float Offset = .3f;
+    public float Velocity = 4f;
+    public Material matieral;
+        
+    private void Start()
+    {
+//        controller.GetComponent<CharacterController>();
+    }
+
+    private void SetHeight()
+    {
+        controller.height = Height;
+    }
+
+    private void SetRadius()
+    {
+        controller.radius = Radius;
+    }
+
+    private void SetOffset()
+    {
+        controller.stepOffset = Offset;
+    }
+
+    private void Center()
+    {
+        controller.center = new Vector3(0, 1, 0); 
+    }
+   
+   
+}
