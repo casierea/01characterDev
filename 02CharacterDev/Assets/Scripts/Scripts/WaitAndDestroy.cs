fb4c6ce71f45bdfd157131d824e6276edaf59549
Unknown <dancingmongoose@gmail.com>
2020-02-09 19:57:49 -0700

    destroy event script, Waitforseconds destroy script

---
+++ 02CharacterDev/Assets/Scripts/Scripts/WaitAndDestroy.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WaitAndDestroy : MonoBehaviour
+{
+   public float SecondsUntilDestroyed;
+
+   IEnumerator Start()
+   {
+      yield return new WaitForSeconds(SecondsUntilDestroyed);
+      Destroy(gameObject);
+   }
+   
+   private void OnCollisionEnter(Collision other)
+   {
+      Destroy(gameObject);
+   }
+}
