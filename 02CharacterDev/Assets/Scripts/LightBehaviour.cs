a01b4a41f0da1bef68ca15b50f2f811b6aa9e329
Unknown <dancingmongoose@gmail.com>
2020-03-02 08:54:50 -0700

    light behaviour script for player candle

---
+++ 02CharacterDev/Assets/Scripts/LightBehaviour.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor.Experimental.UIElements;
+using UnityEngine;
+
+public class lightBehavior : MonoBehaviour
+{
+    public FloatData playerSparkleTotal;
+    public int lossRate = 5;
+    private Boolean lightStatus = false;
+
+    private void Update()
+    {
+        this.GetComponent<Light>().enabled = lightStatus;
+        this.GetComponent<Light>().intensity = playerSparkleTotal.value;
+        this.GetComponent<Light>().range = playerSparkleTotal.value / 10;
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            lightSwitch();
+        }
+
+        if (lightStatus == true)
+        {
+            playerSparkleTotal.value -= lossRate * Time.deltaTime;
+        }
+    }
+    private void lightSwitch()
+    {
+        if (playerSparkleTotal.value > 0)
+        {
+            lightStatus = !lightStatus;
+            this.GetComponent<Light>().enabled = lightStatus;    
+        }
+        else
+        {
+            lightStatus = false;
+        }
+    }
+
+
+}
