2616b6ab9870942343d6ae0078c2336f020efd2b
Casie <dancingmongoose@gmail.com>
2020-03-03 13:10:36 -0700

    dialogue script

---
+++ 02CharacterDev/Assets/StringListData.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+[CreateAssetMenu]
+public class StringListData : ScriptableObject
+{
+    public List<string> stringListObj;
+
+    public int currentLineNumber;
+    //whatever this is in the editot, it will calll that line
+
+    public string ReturnCurrentLine()
+    {
+        return stringListObj[currentLineNumber];
+    }
+
+    public void LineNumber()
+    {
+        if (currentLineNumber < stringListObj.Count-1)
+        {
+            currentLineNumber++;
+        }
+        else
+        {
+            currentLineNumber = 0;
+        }
+        
+    }
+}
