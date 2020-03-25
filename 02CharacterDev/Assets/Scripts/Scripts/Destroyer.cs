768755f809ed957251221a1ca4060783f24498de
Unknown <dancingmongoose@gmail.com>
2020-03-24 21:07:56 -0600

    Destroyer cs

---
+++ 02CharacterDev/Assets/Scripts/Scripts/Destroyer.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+[CreateAssetMenu]
+public class Destroyer : ScriptableObject
+{
+    public void DestroyThis()
+    {
+        Destroy(this);
+    }
+
+    public void DestroyObj(Object obj)
+    {
+        Destroy(obj);
+    }
+
+}
