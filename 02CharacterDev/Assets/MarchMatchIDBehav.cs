ec4f61d841abea290db66cffea6515f54f6d5f8d
Unknown <dancingmongoose@gmail.com>
2020-03-29 17:27:04 -0600

    destroy obj more with collider

--- 02CharacterDev/Assets/MarchMatchIDBehav.cs
+++ 02CharacterDev/Assets/MarchMatchIDBehav.cs
@@ -1,42 +1,42 @@
 ﻿using System;
-using System.Collections.Generic;
-using UnityEngine;
-using UnityEngine.Events;
-
-public class MarchMatchIDBehav: IdBehaviour
-{
-  
-    [Serializable]
-    public struct possibleMatch
-    {
-        public NameId nameIdObj;
-        public UnityEvent workEvent;
-    }
-
-    public List<possibleMatch> nameIdList;
-    [SerializeField]
-    public Dictionary<NameId, UnityEvent> nameIdDictionary;
+ using System.Collections.Generic;
+ using UnityEngine;
+ using UnityEngine.Events;
+ 
+ public class MarchMatchIDBehav: IdBehaviour
+ {
    
-   
-    private NameId otherIdObj;
-   
-    private void OnTriggerEnter(Collider other)
-    {
-        var nameId = other.GetComponent<IdBehaviour>().nameIdObj;
-        if (nameId == null) return;
-      
-        otherIdObj = nameId;
-        CheckId();
-    }
-
-    private void CheckId()
-    {
-        foreach (var obj in nameIdList)
-        {
-            if (otherIdObj == obj.nameIdObj)
-            {
-                obj.workEvent.Invoke();
-            }
-        }
-    }
-}
+     [Serializable]
+     public struct possibleMatch
+     {
+         public NameId nameIdObj;
+         public UnityEvent workEvent;
+     }
+ 
+     public List<possibleMatch> nameIdList;
+     [SerializeField]
+     public Dictionary<NameId, UnityEvent> nameIdDictionary;
+    
+    
+     private NameId otherIdObj;
+    
+     private void OnTriggerEnter(Collider other)
+     {
+         var nameId = other.GetComponent<IdBehaviour>().nameIdObj;
+         if (nameId == null) return;
+       
+         otherIdObj = nameId;
+         CheckId();
+     }
+ 
+     private void CheckId()
+     {
+         foreach (var obj in nameIdList)
+         {
+             if (otherIdObj == obj.nameIdObj)
+             {
+                 obj.workEvent.Invoke();
+             }
+         }
+     }
+ }
