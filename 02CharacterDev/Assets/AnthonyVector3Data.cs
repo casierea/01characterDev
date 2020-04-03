4e79b773fb33794747826608f95292f42f22c0bb
Unknown <dancingmongoose@gmail.com>
2020-04-02 22:26:50 -0600

    datastorage and AnthonyVector3Data

---
+++ 02CharacterDev/Assets/AnthonyVector3Data.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+[CreateAssetMenu(menuName = "Single Variables/Vector3Data")]
+public class AnthonyVector3Data : ScriptableObject
+{
+    public Vector3 value;
+       
+    public void UpdateValue(Transform obj)
+    {
+        //value = obj.TransformPoint(obj.localPosition);
+        value = obj.position;
+    }
+
+    public void UpdateTransform(Transform obj)
+    {
+        obj.localPosition = value;  
+    }
+}
