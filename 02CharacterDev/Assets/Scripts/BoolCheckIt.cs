91871d970f53b220406521767101dfa0bf907040
Unknown <dancingmongoose@gmail.com>
2020-04-08 14:31:54 -0600

    bool check 1 2 1 2

---
+++ 02CharacterDev/Assets/Scripts/BoolCheckIt.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using UnityEngine.Events;
+
+public class BoolCheckIt : MonoBehaviour
+{
+    public FloatData Inventory;
+    public float check;
+    public UnityEvent Open;
+    public UnityEvent Warning;
+
+    public void CheckForItem ()
+    {
+        if (Inventory.value >= check)
+        {
+            Open.Invoke();
+        }
+
+        else
+        {
+            Warning.Invoke();
+        }
+    }
+
+}
