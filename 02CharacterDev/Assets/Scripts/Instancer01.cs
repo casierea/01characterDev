bc0ec9818aa2b0f4625af2fc12ab9e5b398635a2
Unknown <dancingmongoose@gmail.com>
2020-04-16 21:52:34 -0600

    ai and instancer scripts

---
+++ 02CharacterDev/Assets/Scripts/Instancer01.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+
+[CreateAssetMenu]
+public class Instancer01 : ScriptableObject
+{
+    //public GameAction gameActionObj;
+    
+    public Vector3Data instanceAtLocation;
+    
+    public void InstanceObj(GameObject obj)
+    {
+        //var newObj = Instantiate(obj, instanceAtLocation.value, Quaternion.identity);
+        //gameActionObj.transformAction(newObj.transform);
+    }
+}
