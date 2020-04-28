f4193aa09bad53556c47305c7e17e6451ad1598a
Unknown <dancingmongoose@gmail.com>
2020-04-28 12:33:33 -0600

    robot light behaviour and enemy ai

---
+++ 02CharacterDev/Assets/Scripts/EnemyAiPArtol.cs
@@ -0,0 +1,151 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine;
+using UnityEngine.AI;
+
+
+public class EnemyAiPAtrol: MonoBehaviour
+{
+    public List<Vector3Data> PatrolPoints;
+    public GameObject Player;
+    public GameObject Candle;
+    public Boolean playerIsClose = false;
+    public int CurrentDestination = 0;
+    public Boolean DrainOn = false;
+    public Vector3 destination;
+    private NavMeshAgent agent;
+    
+    private void Start()
+    {
+        
+        
+        agent = GetComponent<NavMeshAgent>();
+        destination = PatrolPoints[CurrentDestination].Value;
+    }
+    
+    private void Update() //reorder maybe
+    {
+        agent.destination = destination;
+        checkPlayerDistance();
+        setDestination();
+        
+    }
+
+    private void setDestination()
+    {
+        float destinationDistance = Vector3.Distance(destination, this.GetComponent<Transform>().position);
+
+        if (playerIsClose)
+        {
+            destination = Player.GetComponent<Transform>().position;
+        }
+
+
+        else if (playerIsClose==false & destinationDistance < 1.5)
+        {
+            
+           // Debug.Log(CurrentDestination+ "destination counter");
+            if (CurrentDestination < PatrolPoints.Count -1)
+            {
+                CurrentDestination += 1;    
+            }
+            else
+            {
+                CurrentDestination = 0;
+            }
+            //Debug.Log(CurrentDestination+ " destination counter " + PatrolPoints.Count);
+            destination = PatrolPoints[CurrentDestination].Value;
+        }
+    }
+
+    public void checkPlayerDistance()
+    {
+        var distanceToPlayer =
+            Vector3.Distance(GetComponent<Transform>().position, Player.GetComponent<Transform>().position);
+        //Debug.Log(other.tag);
+        //Debug.Log(other);
+        
+        //add drain and target player if player is close
+        //Debug.Log(distanceToPlayer+ " player distance");
+        if (distanceToPlayer <= Player.GetComponentInChildren<SphereCollider>().radius + 3)
+        {
+
+            playerIsClose = true;
+            //destination = Player.GetComponent<Transform>().position;
+            //Debug.Log("player close " + distanceToPlayer + " " +
+                      //Player.GetComponentInChildren<SphereCollider>().radius);
+            if (DrainOn == false)
+            {
+                //Debug.Log("adding draning");
+                Player.GetComponentInChildren<LightBehaviour>().lossRate += 1;
+                this.GetComponentInChildren<RoboLightBehav>().lossRate -= 1;
+                DrainOn = true;
+            }
+        }
+
+        //remove drain and reset patrol destination if player is not close   
+        if (distanceToPlayer > Player.GetComponentInChildren<SphereCollider>().radius + 10) //light off radius should be .5
+        {
+            playerIsClose = false;
+            //destination = PatrolPoints[CurrentDestination].Value;
+            if (DrainOn == true)
+            {
+                //Debug.Log("removing drain");
+                Player.GetComponentInChildren<LightBehaviour>().lossRate -= 1;
+                this.GetComponentInChildren <RoboLightBehav>().lossRate += 1;
+                DrainOn = false;
+
+            }
+           // Debug.Log("player NOT close "+ distanceToPlayer+" " + Player.GetComponentInChildren<SphereCollider>().radius);
+        }
+
+        
+    }
+    /*
+    public void playerClose(GameObject other)
+    {
+        var distanceToPlayer =
+            Vector3.Distance(GetComponent<Transform>().position, Player.GetComponent<Transform>().position);
+        
+        if (other.tag == "Player" &
+            distanceToPlayer <= Player.GetComponentInChildren<SphereCollider>().radius + 5)
+        {
+            
+            playerIsClose = true;
+            Debug.Log(distanceToPlayer+ "player distance");
+            if (DrainOn == false)
+            {
+                //Debug.Log("adding draning");
+                Player.GetComponentInChildren<LightBehaviour>().lossRate += 1;
+                this.GetComponentInChildren<RoboLightBehav>().lossRate -= 1;
+                DrainOn = true;
+            }
+
+        }
+        
+    }
+    public void playerNotClose(GameObject other)
+    {
+        var distanceToPlayer =
+            Vector3.Distance(GetComponent<Transform>().position, Player.GetComponent<Transform>().position);
+        if (other.tag == "Player" & 
+            distanceToPlayer > Player.GetComponentInChildren<SphereCollider>().radius + 10)
+        {
+            playerIsClose = false;
+            //destination = PatrolPoints[CurrentDestination].Value;
+            if (DrainOn == true)
+            {
+                Debug.Log("removing drain");
+                Player.GetComponentInChildren<LightBehaviour>().lossRate -= 1;
+                this.GetComponentInChildren <RoboLightBehav>().lossRate += 1;
+                DrainOn = false;
+
+            }
+            Debug.Log("player NOT close poop "+ distanceToPlayer);
+        }
+    }
+*/
+
+}
---
+++ 02CharacterDev/Assets/Scripts/LightRobotBehav.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine;
+using System;
+
+public class RoboLightBehav : MonoBehaviour
+{
+    public FloatData SparkleTotal;
+    public int lossRate = 0;
+    private Boolean lightStatus = true;
+
+    //private void Start()
+    // {
+    // SparkleTotal.Value = SparkleTotal.maxValue;
+    //}
+    private void Update()
+    {
+        this.GetComponent<Light>().enabled = lightStatus;
+        this.GetComponent<Light>().intensity = SparkleTotal.value;
+        this.GetComponent<Light>().range = SparkleTotal.value / 10;
+        
+        if (lightStatus == true & SparkleTotal.value > 0)
+        {
+            SparkleTotal.value -= lossRate * Time.deltaTime;
+        }
+        else if (SparkleTotal.value <= 0)
+        {
+            lightStatus = false;
+        }
+    }
+    private void lightSwitch()
+    {
+        if (SparkleTotal.value > 0)
+        {
+            lightStatus = !lightStatus;
+            this.GetComponent<Light>().enabled = lightStatus;    
+        }
+        else
+        {
+            lightStatus = false;
+        }
+    }
+
+
+}
